86de755ffd38251b4b746ad3ac9851cf486f5d67
skytreader <chadestioco@gmail.com>
2013-05-03 18:48:36 +0800

    Small work on unit tests.

--- hashpyre_tests.py
+++ hashpyre_tests.py
@@ -5,12 +5,18 @@ from hashpyre import FileParser
 import unittest
 
 class FileParserTest(unittest.TestCase):
-    
+    """
+    Assume that FileParser's separator is the colon character ':'
+    """
+
     def setUp(self):
+        # TODO Verify with actual redis cli what keys are valid
         self.valid_keys = ("simplekey", "with_underscore",
             "314159265358979", "numb3rm1x", "und3rsc0r3d_31415", "name")
         self.invalid_keys = ("with spacing", "spaced 123")
-
+        
+        self.values = ("simplevalue", "value with spaces", "123", "s1mpl3", "m1x3d spac3",
+            "!@#$^$%^^&", "This is a well constructed sentence.")
 
     def test_key_regex(self):
         for valid in self.valid_keys:
@@ -23,7 +29,8 @@ class FileParserTest(unittest.TestCase):
         self.assertTrue(FileParser.ASSIGNMENT_REGEX.match("name:A Rush of Blood to the Head"))
 
     def test_value_regex(self):
-        self.assertTrue(FileParser.VALUE_REGEX.match("A Rush of Blood to the Head"))
+        for valid in self.values:
+            self.assertTrue(FileParser.VALUE_REGEX.match(valid))
 
 if __name__ == "__main__":
     unittest.main()
