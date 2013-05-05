13f46c41967f9fe0897904183388b277ccac1e69
Chad Estioco <chadestioco@gmail.com>
2013-05-05 16:17:55 +0800

    Unit tests for assignment regex.

--- hashpyre_tests.py
+++ hashpyre_tests.py
@@ -18,6 +18,34 @@ class FileParserTest(unittest.TestCase):
         self.values = ("simplevalue", "value with spaces", "123", "s1mpl3", "m1x3d spac3",
             "!@#$^$%^^&", "This is a well constructed sentence.")
 
+        # Construct the valid and invalid assignment statements
+        self.valid_assignments = []
+        self.invalid_assignments = []
+        self.assignment_separators = [":", " : ", " :"]
+
+        for key in self.valid_keys:
+            for value in self.values:
+                spam = self.generate_assignments(key, value)
+                self.valid_assignments.extend(spam)
+        
+        for key in self.invalid_keys:
+            for value in self.values:
+                spam = self.generate_assignments(key, value)
+                self.invalid_assignments.extend(spam)
+
+    def generate_assignments(self, key, value):
+        """
+        Helper function to generate all assignment test
+        cases (spacing and quoting cases included, although
+        _we don't have quoting cases yet!_).
+        """
+        assignments = []
+        
+        for separator in self.assignment_separators:
+            assignments.append(key + separator + value)
+
+        return assignments
+
     def test_key_regex(self):
         for valid in self.valid_keys:
             self.assertTrue(FileParser.KEY_REGEX.match(valid))
@@ -26,7 +54,11 @@ class FileParserTest(unittest.TestCase):
             self.assertFalse(FileParser.KEY_REGEX.match(invalid))
 
     def test_assignment_regex(self):
-        self.assertTrue(FileParser.ASSIGNMENT_REGEX.match("name:A Rush of Blood to the Head"))
+        for valid in self.valid_assignments:    
+            self.assertTrue(FileParser.ASSIGNMENT_REGEX.match(valid))
+
+        for invalid in self.invalid_assignments:
+            self.assertFalse(FileParser.ASSIGNMENT_REGEX.match(invalid))
 
     def test_value_regex(self):
         for valid in self.values:
